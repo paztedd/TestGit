c9f9f47a276b6f78389e479fc02875280750f3ec
paztedd <777pastet777@mail.ru>
2017-08-26 03:00:14 +0300

    sphere 3 br2

--- Assets/Scripts/Test2.cs
+++ Assets/Scripts/Test2.cs
@@ -6,7 +6,7 @@ public class Test2 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Sphere");
+        Debug.Log("Sphere 3");
 	}
 	
 	// Update is called once per frame
